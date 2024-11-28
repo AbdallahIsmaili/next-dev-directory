6ea6d88463ac154b7fa1f94449539295b0ec1a62
AbdallahIsmaili <aismaili691@gmail.com>
2024-11-28 18:06:27 +0000

    Authentication with NextAuth
    I included GitHub OAuth.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl">Welcome! to NextJS</h1>
+    <>
+      <h1 className="text-3xl">Home Page</h1>
+    </>
   );
 }
