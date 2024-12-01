91938f73b9e349da0d467d6e9c300fecba887d20
AbdallahIsmaili <aismaili691@gmail.com>
2024-12-01 20:17:05 +0000

    Search Form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
+import SearchForm from "@/components/SearchForm";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+
+  const query = (await searchParams).query;
+
   return (
     <>
       <section className="pink_container">
@@ -12,6 +20,8 @@ export default function Home() {
           Share Your Vision, Shape Creative Projects, and Gain Recognition in
           Competitive Challenges.
         </p>
+
+        <SearchForm query={query}/>
       </section>
     </>
   );
