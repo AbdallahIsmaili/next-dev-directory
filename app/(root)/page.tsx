d100faa239a1958f87b7d2eae42281f8aaa6a5b9
AbdallahIsmaili <aismaili691@gmail.com>
2024-12-04 21:41:25 +0000

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({
   searchParams,
@@ -8,6 +9,22 @@ export default async function Home({
 }) {
 
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createDate: new Date(),
+      views: 15,
+      author: {
+        id: 1,
+        name: "Ais",
+      },
+      description: "this is a description",
+      image:
+        "https://images.unsplash.com/photo-1527430253228-e93688616381?q=80&w=1034&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
 
   return (
     <>
@@ -23,6 +40,22 @@ export default async function Home({
 
         <SearchForm query={query}/>
       </section>
+
+      <div className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search result for "${query}"` : 'All Potentials'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts ?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-result">No potential found.</p>
+          )}
+        </ul>
+      </div>
     </>
   );
 }
